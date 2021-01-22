9644b93b93383d640b80598c240455258766f03a
Estheban <developer02pachapp@gmail.com>
2021-01-22 01:46:40 -0500

    perfiles

--- src/components/Odontologo/Pacientes/PacienteRegistro.js
+++ src/components/Odontologo/Pacientes/PacienteRegistro.js
@@ -1,5 +1,96 @@
 import React from 'react';
+import '../../../styles/Odontologo/Pacientes/PacienteRegistro.css'
+
+import add from '../../../assets/images/add.svg'
+import file from '../../../assets/images/file.svg'
+import image from '../../../assets/images/image.svg'
+import save from '../../../assets/images/save.svg'
 
 export const PacienteRegistro = () => {
-	return <div>Aqui registro paciente</div>;
+	return (
+		<div id="pacienteRegistroContenedor" className="paciente-registro-contenedor">
+
+			<h2>Agregar nuevo paciente</h2>
+			<p>Escriba los datos del paciente y adjunte la su historia clinica para completar el perfil.</p>
+			<div className="datos-personales">
+				<h3>Datos Principales</h3>
+				<div className="datos-personales-inputs">
+					<div className="txt-input">
+						<label htmlFor="">Nombres</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Apellidos</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Sexo</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Alergia</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Fecha de Nacimiento</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Dni</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Domicilio</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Ciudad / Distrito / Provincia</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Celular</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Email</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Peso (kg)</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Altura</label>
+						<input type="text" name="" id="" />
+					</div>
+				</div>
+
+			</div>
+			<div className="mas-informacion">
+				<h3>Mas Información</h3>
+				<button id="agregarHistoria" class="agregar-historia" onClick={() => { document.getElementById('agregarHistoriaInput').click() }}>
+
+					<img src={file} alt="file" />
+					<span>Agregar historia clinica</span>
+
+				</button>
+				<button id="agregarImagen" class="agregar-imagen" onClick={() => { document.getElementById('agregarImagenInput').click() }}>
+
+					<img src={image} alt="image" />
+					<span>Subir foto de perfil</span>
+
+				</button>
+
+				<input type="file" id="agregarHistoriaInput" style={{ display: 'none' }} />
+				<input type="file" id="agregarImagenInput" style={{ display: 'none' }} />
+
+				<button class="guardar" >
+
+					<img src={save} alt="add" />
+					<span>Guardar</span>
+
+				</button>
+			</div>
+		</div>
+	);
 };
--- src/components/Odontologo/Perfil/OdontologoPerfil.js
+++ src/components/Odontologo/Perfil/OdontologoPerfil.js
@@ -1,5 +1,59 @@
 import React from 'react';
+import portada from '../../../assets/images/doctor-portada.png'
+import '../../../styles/Odontologo/Perfil/OdontologoPerfil.css'
 
 export const OdontologoPerfil = () => {
-	return <div>Aqui perfil Odontologo</div>;
+	return (
+		<div id="odontologoPerfil" className="odontologo-perfil" >
+			<div className="odontologo-foto" style={{ backgroundImage: `url(${portada})` }}>
+
+				{/* Foto del doctor */}
+				< img src="https://plataformas.news/online/nota_the-good-doctor-posiciona-sony-channel-en-la-region.jpg" alt="" />
+				<button id="changePhoto" onClick={() => { document.getElementById('imgupload').click() }} >
+					Cambiar Foto
+				</button>
+				<input type="file" id="imgupload" />
+			</div>
+			<div className="odontologo-nombre">
+				{/* Cargo */}
+				<h3>Medico Cirujano</h3>
+				{/* Nombre y Apellido */}
+				<h1>Annetter Kathryn</h1>
+				{/* Codigo */}
+				<p>Codigo: <span>6514-1622</span></p>
+			</div>
+			<div className="odontologo-datos">
+				<div className="datos-odontologo-top">
+					<h3>Datos Principales</h3>
+					<a href="/">Ver Portafolio {">"}</a>
+				</div>
+				<div className="datos-odontologo-inputs">
+					<div className="txt-input">
+						<label htmlFor="">Nombre Completo</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Genero</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">DNI</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Email</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">N° de pacientes</label>
+						<input type="text" name="" id="" />
+					</div>
+					<div className="txt-input">
+						<label htmlFor="">Telefono</label>
+						<input type="text" name="" id="" />
+					</div>
+				</div>
+			</div>
+		</div >
+	)
 };
